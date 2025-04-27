f9aaaea4fa583d5556fe4cc2ff9b204a42018b00
Lyhengsun <101932930+Lyhengsun@users.noreply.github.com>
2025-04-27 20:23:17 +0700

    first commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE lumnov_db;
---
+++ src/test/java/com/kshrd/lumnov/LumnovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshrd.lumnov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumnovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
